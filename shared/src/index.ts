1b9f87a9bf7c3a3c8dd2bf9d2ff078ab419e3a28
steven <github@stevenwaterman.uk>
2024-02-24 22:49:10 +0000

    Don't export convertFlatten

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,7 +1,6 @@
-export {finishedData} from "./cleanedData.js";
-export {convertFlatten} from "./coordinates.js";
-export type { Coordinate, Polygon} from "./coordinates.js";
-export {scoreGuess} from "./scoring.js";
+export { finishedData } from "./cleanedData.js";
+export type { Coordinate, Polygon } from "./coordinates.js";
+export { scoreGuess } from "./scoring.js";
 export * as StateInterface from "./serverState.js";
-export {getSongs} from "./songs.js";
+export { getSongs } from "./songs.js";
 export * from "./util.js";
