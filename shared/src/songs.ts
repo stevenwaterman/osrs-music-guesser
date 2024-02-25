d4377c320000ffe6cb35cc98200592e7d111eedb
steven <github@stevenwaterman.uk>
2024-02-25 13:09:39 +0000

    move shuffle into utils

--- shared/src/songs.ts
+++ shared/src/songs.ts
@@ -1,24 +1,5 @@
 import { finishedData } from "./cleanedData.js";
-
-function shuffle<T>(array: T[]): T[] {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
+import { shuffle } from "./util.js";
 
 const allSongs = Object.values(finishedData)
   .filter((song) => song.polygons.length > 0)
--- shared/src/util.ts
+++ shared/src/util.ts
@@ -36,3 +36,23 @@ export function mapValues<InValue, OutValue>(
 export async function sleep(ms: number) {
   return await new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
