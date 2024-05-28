11c3b60a171cdf4a237e5dd65ee08bcf67184b81
Steven Waterman <github@stevenwaterman.uk>
2024-05-28 09:22:05 +0100

    Update HeartbeatWebSocket.ts

--- client/src/lib/HeartbeatWebSocket.ts
+++ client/src/lib/HeartbeatWebSocket.ts
@@ -20,11 +20,9 @@ class ReschedulableTimeout {
 }
 
 export class HeartbeatWebSocket extends WebSocket {
-  private readonly intervalMs = 10_000;
-
-  private clientSendingDataTimer = new ReschedulableTimeout(this.intervalMs);
-  private clientReceivingDataTimer = new ReschedulableTimeout(this.intervalMs);
-  private heartbeatTimeoutTimer = new ReschedulableTimeout(this.intervalMs);
+  private clientSendingDataTimer = new ReschedulableTimeout(25_000);
+  private clientReceivingDataTimer = new ReschedulableTimeout(10_000);
+  private heartbeatTimeoutTimer = new ReschedulableTimeout(5_000);
 
   constructor(url: string) {
     super(url);
